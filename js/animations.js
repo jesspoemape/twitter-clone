41556a6009581437d32932d31e85f2e827a86e4f
Jess Poemape <jesspoemape@gmail.com>
2017-06-08 16:57:31 -0600

    Some progress on the toggle issue

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+// upon page load, make the following hide
+$(".stats, .reply, .tweet-actions").hide();
+
     // upon clicking the text field,
     // text area double in size, and 
     // char count and tweet button appear
@@ -35,10 +38,17 @@ $(document).ready(function() {
      });
 
      // if its a valid tweet, add the tweet to the top of the stream
-     // i guess we are going to clone the element and change the values then prepend to the stream 
+     // clone the element and change the values then prepend to the stream 
      $(".button").on("click", function(){
+         //clear out compose box
+         //$(".tweet-compose").val("");
+         $(".tweet-compose").css("height", "2.5em");
+         $("#tweet-controls").hide();
+
+         // clone tweet stuff
         var $nTweet = $(".tweet:first").clone();
 
+        //reformat tweet stuff
         $(".fullname", $nTweet).text("Jessica Poémape");
         $(".username", $nTweet).text("@japoemape");
         $(".tweet-text", $nTweet).html($(".tweet-compose").val());
@@ -46,6 +56,21 @@ $(document).ready(function() {
         $(".reply .tweet-compose", $nTweet).attr("placeholder", "Reply to @japoemape");
 
         $nTweet.prependTo("#stream");
+
+        // add hover and toggle to new tweets
+        $(".tweet").hover(
+            function(){
+                $(".tweet-actions", this).show();
+            },
+            function() {
+                $(".tweet-actions", this).hide();
+            }
+        );
+
+        $(".tweet").unbind().click(function() {
+        $(".stats, .reply", this).slideToggle("slow");
+         });
+    
      });
 
      //the tweet actions only appear on hover 
@@ -59,7 +84,8 @@ $(document).ready(function() {
      );
 
      // tweet stats only appear on click of tweet 
-     $(".tweet").on("click", function() {
+     $(".tweet").click(function() {
+         console.log("hello");
          $(".stats, .reply", this).slideToggle("slow");
      });
 
